e0c822472583d6f9dd78fabecb077691d8143498
Alex <qwedp@ya.ru>
2020-03-22 18:23:50 +0300

    first commit

---
+++ src/main/java/net/proselyte/crud/model/User.java
@@ -0,0 +1,22 @@
+package net.proselyte.crud.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+}
